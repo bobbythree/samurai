7aa4c0a2bbc8cf0ad304ed1fe69b6e34b52c71c0
bobbythree <bobbythr33@gmail.com>
2025-08-24 22:12:27 -0500

    remove ground animation logic from player class

--- src/Classes/Player.js
+++ src/Classes/Player.js
@@ -1,16 +1,13 @@
 export class Player {
   constructor(character, ground) {
     this.characterImg = character;
-    this.groundGrass = ground;
   }
   update(keyPressed) {
     if (keyPressed['ArrowRight']) {
-      this.groundGrass.classList.add('grass_animate');
       this.characterImg.classList.remove('idle_spritesheet');
       this.characterImg.classList.add('run_spritesheet', "run_animate");
       this.characterImg.src = '../../assets/samurai/Sprites/RUN.png';
     } else {
-      this.groundGrass.classList.remove('grass_animate');
       this.characterImg.classList.remove('run_spritesheet', 'run_animate');
       this.characterImg.classList.add('idle_spritesheet');
       this.characterImg.src = '../../assets/samurai/Sprites/IDLE.png';
