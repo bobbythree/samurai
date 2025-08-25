0ee096f19ecd72e7dde419378388a6d43b710bcd
bobbythree <bobbythr33@gmail.com>
2025-08-24 23:43:14 -0500

    add attack to playerclass

--- src/Classes/Player.js
+++ src/Classes/Player.js
@@ -7,8 +7,12 @@ export class Player {
       this.characterImg.classList.remove('idle_spritesheet');
       this.characterImg.classList.add('run_spritesheet', "run_animate");
       this.characterImg.src = '../../assets/samurai/Sprites/RUN.png';
+    } else if (keyPressed[' ']) {
+      this.characterImg.classList.remove('idle_spritesheet', 'run_spritesheet', 'run_animate');
+      this.characterImg.classList.add('attack_spritesheet', "attack_animate");
+      this.characterImg.src = '../../assets/samurai/Sprites/ATTACK.png';
     } else {
-      this.characterImg.classList.remove('run_spritesheet', 'run_animate');
+      this.characterImg.classList.remove('run_spritesheet', 'run_animate', 'attack_spritesheet', 'attack_animate');
       this.characterImg.classList.add('idle_spritesheet');
       this.characterImg.src = '../../assets/samurai/Sprites/IDLE.png';
     }
